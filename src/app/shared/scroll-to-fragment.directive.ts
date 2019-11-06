58e235154a529df2c72c3e6f59ade762238a2230
Alisa <7586237+alisaduncan@users.noreply.github.com>
2019-11-05 18:04:13 -0600

    Add tests for scroll directive

--- src/app/shared/scroll-to-fragment.directive.ts
+++ src/app/shared/scroll-to-fragment.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { Router, Scroll } from '@angular/router';
+import { NavigationEnd, Router, Scroll } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { PageScrollService } from 'ngx-page-scroll-core';
@@ -10,10 +10,10 @@ import { PageScrollService } from 'ngx-page-scroll-core';
 })
 export class ScrollToFragmentDirective implements OnInit {
 
-  private navScroll: Observable<Scroll>;
+  private navScroll$: Observable<Scroll>;
 
   constructor(router: Router, private scroller: PageScrollService, @Inject(DOCUMENT) private document) {
-    this.navScroll = router.events
+    this.navScroll$ = router.events
       .pipe(
         filter(event => event instanceof Scroll),
         filter((scroll: Scroll) => !!scroll.anchor)
@@ -24,7 +24,7 @@ export class ScrollToFragmentDirective implements OnInit {
     // JAD Note- There is a bug when navigating between fragments across 2 different routes with calculating element scroll
     // https://github.com/angular/angular/issues/30139
     // We can work around this by  subscribing to router event and using setTimeout to scroll :(
-    this.navScroll.subscribe( (scroll: Scroll) => {
+    this.navScroll$.subscribe( (scroll: Scroll) => {
       setTimeout(() => {
         this.scroller.scroll({ document: this.document, scrollTarget: `#${scroll.anchor}`});
       });

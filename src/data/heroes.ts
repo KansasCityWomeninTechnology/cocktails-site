4e1bde350fbe38e5d60f7531522595ddaf408417
Gabi Dombrowski <gabdombrowski@gmail.com>
2020-03-02 09:10:40 -0600

    Update Twitter handle for Joy

--- src/data/heroes.ts
+++ src/data/heroes.ts
@@ -1,151 +1,180 @@
 export interface Hero {
-    name: string;
-    title: string;
-    img: string;
-    twitter: string;
+  name: string;
+  title: string;
+  img: string;
+  twitter: string;
 }
 
-export const LEADERS: Hero[] = [{
-    name: 'Jennifer Wadella',
-    title: 'Founder',
-    img: 'assets/images/mentors/jennifer.png',
-    twitter: 'likeOMGitsFEDAY'
-}, {
-    name: 'Rachel Hagan',
-    title: 'Program Director',
-    img: 'assets/images/mentors/rachelh.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Amy Norris',
-    title: 'Co Director',
-    img: 'assets/images/mentors/amy.jpg',
-    twitter: 'amynorris'
-}, {
-    name: 'Kayla Hennegin',
-    title: 'Curriculum Director',
-    img: 'assets/images/mentors/kayla.png',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Gabi Dombrowski',
-    title: 'Mentor Director',
-    img: 'assets/images/mentors/gabi.jpg',
-    twitter: 'gabdom'
-}, {
-    name: 'Alisa Duncan',
-    title: 'Technical Materials Director',
-    img: 'assets/images/mentors/alisa.jpg',
-    twitter: 'AlisaDuncan'
-}, {
-    name: 'Andie Johnson',
-    title: 'Marketing Director',
-    img: 'assets/images/mentors/andie.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Kallie Riff',
-    title: 'Presentation Director',
-    img: 'assets/images/mentors/kallie.jpg',
-    twitter: 'KallieRiff'
-}];
+export const LEADERS: Hero[] = [
+  {
+    name: "Jennifer Wadella",
+    title: "Founder",
+    img: "assets/images/mentors/jennifer.png",
+    twitter: "likeOMGitsFEDAY"
+  },
+  {
+    name: "Rachel Hagan",
+    title: "Program Director",
+    img: "assets/images/mentors/rachelh.jpg",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Amy Norris",
+    title: "Co Director",
+    img: "assets/images/mentors/amy.jpg",
+    twitter: "amynorris"
+  },
+  {
+    name: "Kayla Hennegin",
+    title: "Curriculum Director",
+    img: "assets/images/mentors/kayla.png",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Gabi Dombrowski",
+    title: "Mentor Director",
+    img: "assets/images/mentors/gabi.jpg",
+    twitter: "gabdom"
+  },
+  {
+    name: "Alisa Duncan",
+    title: "Technical Materials Director",
+    img: "assets/images/mentors/alisa.jpg",
+    twitter: "AlisaDuncan"
+  },
+  {
+    name: "Andie Johnson",
+    title: "Marketing Director",
+    img: "assets/images/mentors/andie.jpg",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Kallie Riff",
+    title: "Presentation Director",
+    img: "assets/images/mentors/kallie.jpg",
+    twitter: "KallieRiff"
+  }
+];
 
-
-export const MENTORS: Hero[] = [{
-    name: 'Tamara Copple',
-    title: 'Mentor',
-    img: 'assets/images/mentors/tamara.png',
-    twitter: 'Ceiligirl72'
-}, {
-    name: 'Heather Pekrul',
-    title: 'Mentor',
-    img: 'assets/images/mentors/heatherp.jpg',
-    twitter: 'heatherpekrul'
-}, {
-    name: 'Maroua Jawadi',
-    title: 'Mentor',
-    img: 'assets/images/mentors/maroua.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Rhia Dixon',
-    title: 'Mentor',
-    img: 'assets/images/mentors/rhia.jpg',
-    twitter: 'TheFakeRiRi'
-}, {
-    name: 'Melanie Garvey',
-    title: 'Mentor',
-    img: 'assets/images/mentors/mel.jpg',
-    twitter: 'MelanieGarvey'
-}, {
-    name: 'McKenzie Hine',
-    title: 'Mentor',
-    img: 'assets/images/mentors/mckenzie.jpg',
-    twitter: 'mckenz_h'
-}, {
-    name: 'Jenn Bono',
-    title: 'Mentor',
-    img: 'assets/images/mentors/jenn.jpg',
-    twitter: 'jenn_bono24'
-}, {
-    name: 'Erin Wilson',
-    title: 'Mentor',
-    img: 'assets/images/mentors/erin.jpg',
-    twitter: 'erin_wilson92'
-}, {
-    name: 'Gabby Ortman',
-    title: 'Mentor',
-    img: 'assets/images/mentors/gabby.jpg',
-    twitter: 'gabbyortman'
-}, {
-    name: 'Jessica Kincaid',
-    title: 'Mentor',
-    img: 'assets/images/mentors/jessica.jpg',
-    twitter: 'jessica_k1ncaid'
-}, {
-    name: 'Gloria Higley',
-    title: 'Mentor',
-    img: 'assets/images/mentors/gloria.jpg',
-    twitter: 'gloriahigley'
-}, {
-    name: 'Sarah Duitsman',
-    title: 'Mentor',
-    img: 'assets/images/mentors/sarah.png',
-    twitter: 'skduits'
-}, {
-    name: 'Meg O\'Roark',
-    title: 'Mentor',
-    img: 'assets/images/mentors/meg.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Melissa Arroyo',
-    title: 'Mentor',
-    img: 'assets/images/mentors/melissa.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Melissa Waltner',
-    title: 'Mentor',
-    img: 'assets/images/mentors/melissaw.jpg',
-    twitter: 'suitemel'
-}, {
-    name: 'Jacqueline Kolze',
-    title: 'Mentor',
-    img: 'assets/images/mentors/jacqueline.jpg',
-    twitter: 'just_jacquelin'
-}, {
-    name: 'Liz Wait',
-    title: 'Mentor',
-    img: 'assets/images/mentors/liz.jpg',
-    twitter: 'auraswap'
-}, {
-    name: 'Keen Wilson',
-    title: 'Mentor',
-    img: 'assets/images/mentors/keen.jpg',
-    twitter: 'youaresokeen'
-}, {
-    name: 'Joy Circo',
-    title: 'Mentor',
-    img: 'assets/images/mentors/joy.jpg',
-    twitter: 'codecocktailskc'
-}, {
-    name: 'Malerie Pace',
-    title: 'Mentor',
-    img: 'assets/images/mentors/malerie.jpg',
-    twitter: 'codecocktailskc'
-}];
+export const MENTORS: Hero[] = [
+  {
+    name: "Tamara Copple",
+    title: "Mentor",
+    img: "assets/images/mentors/tamara.png",
+    twitter: "Ceiligirl72"
+  },
+  {
+    name: "Heather Pekrul",
+    title: "Mentor",
+    img: "assets/images/mentors/heatherp.jpg",
+    twitter: "heatherpekrul"
+  },
+  {
+    name: "Maroua Jawadi",
+    title: "Mentor",
+    img: "assets/images/mentors/maroua.jpg",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Rhia Dixon",
+    title: "Mentor",
+    img: "assets/images/mentors/rhia.jpg",
+    twitter: "TheFakeRiRi"
+  },
+  {
+    name: "Melanie Garvey",
+    title: "Mentor",
+    img: "assets/images/mentors/mel.jpg",
+    twitter: "MelanieGarvey"
+  },
+  {
+    name: "McKenzie Hine",
+    title: "Mentor",
+    img: "assets/images/mentors/mckenzie.jpg",
+    twitter: "mckenz_h"
+  },
+  {
+    name: "Jenn Bono",
+    title: "Mentor",
+    img: "assets/images/mentors/jenn.jpg",
+    twitter: "jenn_bono24"
+  },
+  {
+    name: "Erin Wilson",
+    title: "Mentor",
+    img: "assets/images/mentors/erin.jpg",
+    twitter: "erin_wilson92"
+  },
+  {
+    name: "Gabby Ortman",
+    title: "Mentor",
+    img: "assets/images/mentors/gabby.jpg",
+    twitter: "gabbyortman"
+  },
+  {
+    name: "Jessica Kincaid",
+    title: "Mentor",
+    img: "assets/images/mentors/jessica.jpg",
+    twitter: "jessica_k1ncaid"
+  },
+  {
+    name: "Gloria Higley",
+    title: "Mentor",
+    img: "assets/images/mentors/gloria.jpg",
+    twitter: "gloriahigley"
+  },
+  {
+    name: "Sarah Duitsman",
+    title: "Mentor",
+    img: "assets/images/mentors/sarah.png",
+    twitter: "skduits"
+  },
+  {
+    name: "Meg O'Roark",
+    title: "Mentor",
+    img: "assets/images/mentors/meg.jpg",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Melissa Arroyo",
+    title: "Mentor",
+    img: "assets/images/mentors/melissa.jpg",
+    twitter: "codecocktailskc"
+  },
+  {
+    name: "Melissa Waltner",
+    title: "Mentor",
+    img: "assets/images/mentors/melissaw.jpg",
+    twitter: "suitemel"
+  },
+  {
+    name: "Jacqueline Kolze",
+    title: "Mentor",
+    img: "assets/images/mentors/jacqueline.jpg",
+    twitter: "just_jacquelin"
+  },
+  {
+    name: "Liz Wait",
+    title: "Mentor",
+    img: "assets/images/mentors/liz.jpg",
+    twitter: "auraswap"
+  },
+  {
+    name: "Keen Wilson",
+    title: "Mentor",
+    img: "assets/images/mentors/keen.jpg",
+    twitter: "youaresokeen"
+  },
+  {
+    name: "Joy Circo",
+    title: "Mentor",
+    img: "assets/images/mentors/joy.jpg",
+    twitter: "andthenjoysaid"
+  },
+  {
+    name: "Malerie Pace",
+    title: "Mentor",
+    img: "assets/images/mentors/malerie.jpg",
+    twitter: "codecocktailskc"
+  }
+];

0a1d6b6ce8876b269a064c1b6d360e3eb378665a
Alisa Duncan <7586237+alisaduncan@users.noreply.github.com>
2020-10-09 08:12:41 -0500

    fix lint errors

--- src/app/shared/map/google-maps.service.spec.ts
+++ src/app/shared/map/google-maps.service.spec.ts
@@ -5,6 +5,8 @@ import { googleMock } from './testing/fake-google';
 import { Coordinate, MarkerAnimation } from './map-types';
 import { ElementRef } from '@angular/core';
 
+// There are deprecated called to TestBed.get. Instead of fixing these, I am planning on removing the Google Maps component
+// and using the one now available from Google Components lib
 describe('GoogleMapsService', () => {
   let googleMockSpy: jasmine.Spy;
   let service: GoogleMapsService;

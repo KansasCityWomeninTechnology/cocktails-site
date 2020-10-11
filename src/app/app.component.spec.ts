7dd52bae3743396215a66c08266800dc248fdda0
Cutie on the couch <sjpcp5@gmail.com>
2020-10-10 19:34:53 -0500

    replace async with waitForAsync app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, waitForAsync(), ComponentFixture } from '@angular/core/testing';
+import { TestBed, waitForAsync, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
@@ -6,7 +6,7 @@ describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(waitForAsync()(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
@@ -21,11 +21,11 @@ describe('AppComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create the app', waitForAsync()(() => {
+  it('should create the app', waitForAsync(() => {
     expect(component).toBeTruthy();
   }));
 
-  it(`should have as title 'Coding & Cocktails by Kansas City Women in Technology'`, waitForAsync()(() => {
+  it(`should have as title 'Coding & Cocktails by Kansas City Women in Technology'`, waitForAsync(() => {
     expect(component.title).toEqual('Coding & Cocktails by Kansas City Women in Technology');
   }));
 });

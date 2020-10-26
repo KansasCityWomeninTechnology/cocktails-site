d2a42a3beb36f724924560ec8088653a57862f92
Tara Dunmore <30328414+qualmless@users.noreply.github.com>
2020-10-25 19:34:56 -0500

    issue 111 changes made

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeroComponent } from './hero.component';
 import { By } from '@angular/platform-browser';
@@ -7,7 +7,7 @@ describe('HeroComponent', () => {
   let component: HeroComponent;
   let fixture: ComponentFixture<HeroComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroComponent ]
     })

da72e20201ea25b81e97e4dcb1901db68b6e86c1
Rachel Hagan <rachagan21@gmail.com>
2021-04-06 16:16:05 -0500

    angular series links

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -57,20 +57,20 @@ export const SESSIONS: Session[] = [{
     date: '2021-04-10',
     time: '7-9pm',
     name: 'Coding & Cocktails: Angular Pt. 2: Playing with Blocks (Angular Components)',
-    eventbriteLink: 'https://www.eventbrite.com/o/coding-amp-cocktails-kansas-city-8343130945',
-    eventbriteIFrameLink: ''
+    eventbriteLink: 'https://www.eventbrite.com/e/angular-pt-2-playing-with-blocks-angular-components-tickets-142441220525?aff=ccweb',
+    eventbriteIFrameLink: '//eventbrite.com/tickets-external?eid=142441220525&ref=ccweb'
 }, {
     date: '2021-05-08',
     time: '7-9pm',
     name: 'Coding & Cocktails: Angular Pt. 3: Fake It ’Til You Make It (Data Binding & DOM Manipulation)',
-    eventbriteLink: 'https://www.eventbrite.com/o/coding-amp-cocktails-kansas-city-8343130945',
-    eventbriteIFrameLink: ''
+    eventbriteLink: 'https://www.eventbrite.com/e/angular-pt-3-fake-it-til-you-make-it-data-binding-dom-manipulation-tickets-142441423131?aff=ccweb',
+    eventbriteIFrameLink: '//eventbrite.com/tickets-external?eid=142441423131&ref=ccweb'
 }, {
     date: '2021-06-12',
     time: '7-9pm',
     name: 'Coding & Cocktails: Angular Pt. 4: Go Fetch (APIs)',
-    eventbriteLink: 'https://www.eventbrite.com/o/coding-amp-cocktails-kansas-city-8343130945',
-    eventbriteIFrameLink: ''
+    eventbriteLink: 'https://www.eventbrite.com/e/angular-pt-4-go-fetch-apis-tickets-142441537473?aff=ccweb',
+    eventbriteIFrameLink: '//eventbrite.com/tickets-external?eid=142441537473&ref=ccweb'
 }, {
     date: '2021-07-10',
     time: '6:00-7:30:pm',
@@ -104,4 +104,4 @@ export const SESSIONS: Session[] = [{
 }];
 
 // Month to display for upcoming session. Use 7 for July, 8 for Aug, etc
-export const UPCOMING_SESSION_MONTH = 3;
+export const UPCOMING_SESSION_MONTH = 4;

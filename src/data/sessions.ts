58b2b7f07cdd7e6b3f2c2d29a2d98521607bafd5
Hyunsu Kim <hyunsujk@gmail.com>
2022-02-26 19:56:51 -0600

    update worksheet link (#183)
    update worksheet link

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -1,27 +1,39 @@
 export interface Topic {
-    name: string;
-    slidesLink: string;
-    worksheetLink: string;
-    areLinksEnabled: boolean;
+  name: string;
+  slidesLink: string;
+  worksheetLink: string;
+  areLinksEnabled: boolean;
 }
 
-export const TOPICS: Topic[] = [];
+export const TOPICS: Topic[] = [
+  {
+    name: 'Intro to HTML',
+    slidesLink: '',
+    worksheetLink:
+      'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/',
+    areLinksEnabled: true,
+  },
+];
 
 export interface Session {
-    date: string;
-    time: string;
-    name: string;
-    eventbriteLink: string;
-    eventbriteIFrameLink: string;
+  date: string;
+  time: string;
+  name: string;
+  eventbriteLink: string;
+  eventbriteIFrameLink: string;
 }
 
-export const SESSIONS: Session[] = [{
+export const SESSIONS: Session[] = [
+  {
     date: '2022-02-12',
     time: '7-9pm',
     name: 'Coding & Cocktails: Next Level GitHub Profiles - ONLINE EVENT',
-    eventbriteLink: 'https://www.eventbrite.com/e/coding-cocktails-next-level-github-profiles-tickets-236232948107?aff=ccweb',
-    eventbriteIFrameLink: '//eventbrite.com/tickets-external?eid=236232948107&ref=ccweb'
-}];
+    eventbriteLink:
+      'https://www.eventbrite.com/e/coding-cocktails-next-level-github-profiles-tickets-236232948107?aff=ccweb',
+    eventbriteIFrameLink:
+      '//eventbrite.com/tickets-external?eid=236232948107&ref=ccweb',
+  },
+];
 
 // Month to display for upcoming session. Use 7 for July, 8 for Aug, etc
 // the minus one is because we skipped July

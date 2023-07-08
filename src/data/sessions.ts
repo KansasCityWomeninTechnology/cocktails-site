2fd16f55b7215adb6cc368952a35572de4dd0d49
Nessa Shultz <95943218+vanessashultz@users.noreply.github.com>
2023-07-08 15:38:42 -0500

    Vs css 2 (#246)

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -6,21 +6,21 @@ export interface Topic {
 }
 
 export const TOPICS: Topic[] = [
-  {
-    name: 'Intro to HTML',
-    slidesLink:
-      'https://kansascitywomenintechnology.github.io/CodingCocktailsSlideTemplate/01-glasswork/2023-front-end-html.html#/',
-    worksheetLink:
-      'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/?id=worksheets',
-    areLinksEnabled: true,
-  },
+//   {
+//     name: 'Intro to HTML',
+//     slidesLink:
+//       'https://kansascitywomenintechnology.github.io/CodingCocktailsSlideTemplate/01-glasswork/2023-front-end-html.html#/',
+//     worksheetLink:
+//       'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/?id=worksheets',
+//     areLinksEnabled: true,
+//   },
   {
     name: 'CSS Basics',
     slidesLink:
       'https://kansascitywomenintechnology.github.io/CodingCocktailsSlideTemplate/css-series/2023-css.html#/',
     worksheetLink:
       'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/?id=worksheets',
-    areLinksEnabled: false,
+    areLinksEnabled: true,
   },
   // {
   //   name: 'Intro to JavaScript',

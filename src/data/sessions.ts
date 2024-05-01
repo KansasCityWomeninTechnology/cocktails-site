1e41617507496d76467fa2badee2a537f5d655bc
Alex Herron <113468236+alexeherron@users.noreply.github.com>
2024-05-01 11:35:50 -0500

    Update sessions.ts

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -12,7 +12,7 @@ export const TOPICS: Topic[] = [
       'https://kansascitywomenintechnology.github.io/CodingCocktailsSlideTemplate/01-glasswork/2023-front-end-html.html#/',
     worksheetLink:
       'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/?id=worksheets',
-    areLinksEnabled: false,
+    areLinksEnabled: true,
   },
   {
     name: 'CSS Basics',
@@ -20,7 +20,7 @@ export const TOPICS: Topic[] = [
       'https://kansascitywomenintechnology.github.io/CodingCocktailsSlideTemplate/css-series/2023-css.html#/',
     worksheetLink:
       'https://kansascitywomenintechnology.github.io/cocktails-worksheets/#/?id=worksheets',
-    areLinksEnabled: false,
+    areLinksEnabled: true,
   },
   {
     name: 'Intro to JavaScript',
